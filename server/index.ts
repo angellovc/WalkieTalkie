4e0641e218fb3bdc180d6b031814df67edd1ea7d
angello-villegas <avillegas@k12.com>
2022-07-09 13:18:44 -0500

    index.ts (updated): unnecessary console.log was deleted on the chat listener, to keep the users communication private and the connection console.log was modified to be more explicite

--- server/index.ts
+++ server/index.ts
@@ -10,13 +10,12 @@ if (process.env.WALKIETALKIE_PORT === undefined )
     throw Error('WALKIETALKIE_PORT env variable should exists')
 
 io.on('connection', socket => {
-    console.log('user connection established');
     const channel = `r${Math.floor(Math.random() * 10)}-${shortid.generate()}`;
     const userId = shortid.generate();
+    console.log(`user ${userId} connection established`);
     socket.emit('session', {channel, userId});
 
     socket.on('chat', (channel, data) => {
-        console.log(channel, data)
         socket.broadcast.emit(channel, data);
     });
 
